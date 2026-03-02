b8a5a8fef59d8b7da78a350a7f0fce9e7f6c3175
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-03-02 03:33:09 +0300

    refactor: update names, cleanup code (#10)
    * Remove unused flush
    
    * Rename DBSession to TransactionManager

--- book_club/application/interfaces.py
+++ book_club/application/interfaces.py
@@ -19,9 +19,6 @@ class UUIDGenerator(Protocol):
     def __call__(self) -> UUID: ...
 
 
-class DBSession(Protocol):
+class TransactionManager(Protocol):
     @abstractmethod
     async def commit(self) -> None: ...
-
-    @abstractmethod
-    async def flush(self) -> None: ...
